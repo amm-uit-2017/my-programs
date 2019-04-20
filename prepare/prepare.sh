eed19aea38c574ef0c0e1c31444d7d75add245e5
Aung Myint Myat <amm.uit.2017@gmail.com>
2019-04-20 11:34:56 +0630

    Update prepare.sh

--- prepare/prepare.sh
+++ prepare/prepare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rc=$(curl -k https://raw.githubusercontent.com/amm-uit-2017/my-programs/master/prepare/vimrc)
+wget https://raw.githubusercontent.com/amm-uit-2017/my-programs/master/prepare/vimrc
 wget https://raw.githubusercontent.com/amm-uit-2017/my-programs/master/prepare/ayu.vim
 
-echo "$rc" >> /etc/vim/vimrc
+mv vimrc ~/.vimrc
 
-mkdir -p /etc/vim/colors
-mv ayu.vim /etc/vim/colors/
+mkdir -p ~/.vim/colors
+mv ayu.vim ~/.vim/colors/
 
 echo "set bell-style none" >> /etc/inputrc
