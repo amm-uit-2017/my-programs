e8099118993891aaa930932a83c4ca38b25c2d95
Aung Myint Myat <amm.uit.2017@gmail.com>
2019-04-28 17:04:41 +0630

    Update prepare.sh

--- prepare/prepare.sh
+++ prepare/prepare.sh
@@ -1,16 +1,77 @@
 #!/bin/bash
 
-echo "########## Installing Basic Programs #########"
-yum install wget vim net-tools git tcpdump
+#################### Functions ####################
+function install_programs (){
+	echo "########## Installing Programs ##########";
+	echo "Net-tools, Wget, Vim, Git, Tcpdump, NTP-client will be installed."
+	# yum install wget vim net-tools git tcpdump ntp
+	sleep 1
+}
 
-echo "########## Installing Vim Theme #########"
+function adjust_time (){
+	echo "########## Adjusting Time ##########";
+	sleep 1
+	# ln -sf /usr/share/zoneinfo/Asia/Yangon /etc/localtime
+}
 
-wget https://raw.githubusercontent.com/amm-uit-2017/my-programs/master/prepare/vimrc
-wget https://raw.githubusercontent.com/amm-uit-2017/my-programs/master/prepare/ayu.vim
+function vim_theme (){
+	echo "########## Installing Vim Theme #########"
+	sleep 1
 
-mv vimrc ~/.vimrc
+	# wget https://raw.githubusercontent.com/amm-uit-2017/my-programs/master/prepare/vimrc
+	# wget https://raw.githubusercontent.com/amm-uit-2017/my-programs/master/prepare/ayu.vim
 
-mkdir -p ~/.vim/colors
-mv ayu.vim ~/.vim/colors/
+	# mv vimrc ~/.vimrc
 
-echo "set bell-style none" >> /etc/inputrc
+	# mkdir -p ~/.vim/colors
+	# mv ayu.vim ~/.vim/colors/
+
+	# echo "set bell-style none" >> /etc/inputrc
+}
+
+function do_all (){
+
+	install_programs
+	adjust_time
+	vim_theme
+
+}
+
+function dun_know (){
+	echo "########## I don't understand #########"
+}
+
+function quit (){
+	echo "########## Exiting #########"
+}
+
+while true; do
+
+	cat << EOF
+
+
+	Choose Your Desire Option
+	===========================================
+	(1) Install Basic Packages
+	(2) Adjust Time"
+	(3) Configuring Vim Theme
+	(4) Do All Steps
+	(0) Quit
+
+EOF
+
+	################### Accept Input ######################
+	echo -n "Type the number: "
+	read input
+
+	case $input in
+		1) install_programs ;;
+		2) adjust_time ;;
+		3) vim_theme ;;
+		4) do_all ;;
+		0) quit quit; break;;
+		*) dun_know ;;
+
+	esac
+
+done
